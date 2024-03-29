802d4c007cbc43ffa3cbe19ea2eb1c33326614c9
Kulvir Singh <ksingh11_be20@thapar.edu>
2024-03-29 11:29:59 +0530

    ✅ Capture image and annotate

---
+++ capture_images.py
@@ -0,0 +1,38 @@
+import os
+import cv2
+import time
+import uuid
+
+IMAGE_PATH = "dataset"
+
+labels = ["Hello", "Yes", "No", "Thanks", "IloveYou", "Please"]
+
+num_images = 20
+
+for label in labels:
+    img_path = os.path.join(IMAGE_PATH, label)
+    os.makedirs(img_path)
+
+    # Use the camera to capture images
+    cap = cv2.VideoCapture(0)
+    print(f"Collecting images for {label}")
+
+    # 5 sec delay to get ready
+    time.sleep(5)
+
+    for num_img in range(num_images):
+        ret, frame = cap.read()
+
+        name = f"label + '.' + '{str(uuid.uuid1())}.jpg'"
+        image_name = os.path.join(IMAGE_PATH, label, name)
+
+        cv2.imwrite(image_name, frame)
+        cv2.imshow("frame", frame)
+
+        time.sleep(2)
+        
+        # press 'q' to quit
+        if cv2.waitKey(1) & 0xFF==ord("q"):
+            break
+
+    cap.release()
